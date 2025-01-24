05cf14b77e5c3b1f58eb55f4bd9aefd7485b6691
pdimitrop <67342384+pdimitrop@users.noreply.github.com>
2025-01-24 14:32:28 +0200

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,16 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tron.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
+    public void OnGet()
         {
 
         }
